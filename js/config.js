4ad64dd5759abb241ea82ff8d1929f09f4c59dfc
Ebenezer <bamisayebenezer@gmail.com>
2023-06-09 19:00:38 +0100

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_CXpL1DudV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'mvuas17g1baqs5iuse06t77nk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_fym7fMvwE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '37pimtogsvmn6fr2vjvnag7ubm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
